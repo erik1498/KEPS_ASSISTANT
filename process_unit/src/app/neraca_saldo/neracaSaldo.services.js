0c3083daffaee94c41ab7e32671428e7d5e25e68
erik1498 <djefrisonferdinan@gmail.com>
2024-05-03 17:23:47 +0800

    Update Sum Result Neraca Saldo

--- process_unit/src/app/neraca_saldo/neracaSaldo.services.js
+++ process_unit/src/app/neraca_saldo/neracaSaldo.services.js
@@ -10,38 +10,14 @@ export const getAllNeracaSaldoByBulanService = async (bulan, tahun, whereIN, req
     LOGGER(logType.INFO, `Start getAllNeracaSaldoByBulanService [${bulan} ${tahun}]`, whereIN, req_identity)
     let jurnalUmumBulanSebelum = await convertJurnalUmumToNeracaSaldo(await getJurnalUmumByBulanSebelumService(bulan, tahun, req_identity))
     let historyAkuns = await getNeracaSaldoByBulanRepo(bulan, tahun, whereIN)
-    // const fakturPenjualanBarang = await getFakturPenjualanBarangByBulanRepo(tahun, bulan)
-    // const pelunasanPenjualanBarang = await getPelunasanPenjualanBarangByBulanRepo(tahun, bulan)
-    // const returPenjualanBarang = await getReturPenjualanBarangByBulanRepo(tahun, bulan)
-    // const fakturPelunasanAndReturPenjualanBarangToNeracaSaldo = await getFakturPelunasanAndReturPenjualanBarangToNeracaSaldo({
-    //     fakturPenjualanBarang, returPenjualanBarang, pelunasanPenjualanBarang
-    // })
-
-    // const fakturPenjualanJasa = await getFakturPenjualanJasaByBulanRepo(tahun, bulan)
-    // const pembatalanPenjualanJasa = await getPembatalanPenjualanJasaByBulanRepo(tahun, bulan)
-    // const fakturPelunasanAndReturPenjualanJasaToNeracaSaldo = await getFakturPelunasanAndReturPenjualanJasaToNeracaSaldo({
-    //     fakturPenjualanJasa, pembatalanPenjualanJasa
-    // })
-
-
-    // fakturPelunasanAndReturPenjualanBarangToNeracaSaldo
-    // .concat(...fakturPelunasanAndReturPenjualanJasaToNeracaSaldo)
-    // .map((item) => {
-    //     const getHistory = historyAkuns.filter((history) => history.kode_akun_perkiraan_code == item.kode_akun_perkiraan_code)
-    //     const index = historyAkuns.indexOf(getHistory.length > 0 ? getHistory[0] : null)
-    //     if (index >= 0) {
-    //         historyAkuns[index].sum_result += item.sum_result
-    //     }else{
-    //         historyAkuns.push(item)
-    //     }
-    // })
 
     jurnalUmumBulanSebelum.map((item) => {
         const getHistory = historyAkuns.filter((history) => history.kode_akun_perkiraan_code == item.kode_akun_perkiraan_code)
         const index = historyAkuns.indexOf(getHistory.length > 0 ? getHistory[0] : null)
         if (index >= 0) {
-            historyAkuns[index].sum_result = getSumOfStringValue([historyAkuns[index].sum_result, item.sum_result])
+            historyAkuns[index].sum_result = getSumOfStringValue([historyAkuns[index].sum_result, item.sum_result]).toFixed(2)
         }else{
+            item.sum_result = item.sum_result.toFixed(2);
             historyAkuns.push(item)
         }
     })
