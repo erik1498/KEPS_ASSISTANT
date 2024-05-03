56af7e1f34b9fc0a01697c9a50592e418e453370
erik1498 <djefrisonferdinan@gmail.com>
2024-05-03 15:58:55 +0800

    Update Package.JSON and LoggerUtils

--- process_unit/src/utils/loggerUtil.js
+++ process_unit/src/utils/loggerUtil.js
@@ -1,5 +1,5 @@
-import pino from "pino";
-import moment from "moment"
+// import pino from "pino";
+// import moment from "moment"
 import { createLoggerService } from "../app/logger/logger.services.js";
 
 export const logType = {
@@ -24,36 +24,36 @@ export const LOGGER = (logType, message, object, req_identity, req_original_url,
     message = "[ ID:" + req_identity + " ] = " + message
     message = object ? message + " [ " + JSON.stringify(object, null, 4) + " ]" : message
 
-    if (logType == "info") {
-        pinoLogConfig.info(message)
-    }
-    if (logType == "error") {
-        pinoLogConfig.error(message)
-        if (error_monitor) { LOGGER_MONITOR(req_original_url, req_method, object, req_identity, true) }
-    }
-    if (logType == "debug") {
-        pinoLogConfig.debug(message)
-    }
+    // if (logType == "info") {
+    //     pinoLogConfig.info(message)
+    // }
+    // if (logType == "error") {
+    //     pinoLogConfig.error(message)
+    //     if (error_monitor) { LOGGER_MONITOR(req_original_url, req_method, object, req_identity, true) }
+    // }
+    // if (logType == "debug") {
+    //     pinoLogConfig.debug(message)
+    // }
 }
-let date_ob = new Date();
+// let date_ob = new Date();
 
-export const pinoLogConfig = pino(
-    {
-        timestamp: () => `, "time":"${moment().format()}"`,
-        transport: {
-            targets: [
-                {
-                    target: "pino-pretty"
-                },
-                // {
-                //     // target:"pino/file",
-                //     target:"pino-pretty",
-                //     options: {
-                //         destination: `./log/${(date_ob.getDate() < 10 ? "0" + date_ob.getDate() : date_ob.getDate()) + "" + ((date_ob.getMonth() + 1) < 10 ? "0" + (date_ob.getMonth() + 1) : (date_ob.getMonth() + 1)) + "" + date_ob.getFullYear()}.pinoLogConfig`,
-                //         mkdir:true,
-                //     }
-                // }
-            ]
-        },
-    },
-)
+// export const pinoLogConfig = pino(
+//     {
+//         timestamp: () => `, "time":"${moment().format()}"`,
+//         transport: {
+//             targets: [
+//                 {
+//                     target: "pino-pretty"
+//                 },
+//                 // {
+//                 //     // target:"pino/file",
+//                 //     target:"pino-pretty",
+//                 //     options: {
+//                 //         destination: `./log/${(date_ob.getDate() < 10 ? "0" + date_ob.getDate() : date_ob.getDate()) + "" + ((date_ob.getMonth() + 1) < 10 ? "0" + (date_ob.getMonth() + 1) : (date_ob.getMonth() + 1)) + "" + date_ob.getFullYear()}.pinoLogConfig`,
+//                 //         mkdir:true,
+//                 //     }
+//                 // }
+//             ]
+//         },
+//     },
+// )
