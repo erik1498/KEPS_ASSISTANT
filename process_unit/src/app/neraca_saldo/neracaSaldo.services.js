0a320d44dc9c3bf6598d862092c7ca1376935f0d
erik1498 <djefrisonferdinan@gmail.com>
2024-05-03 23:41:41 +0800

    Update Neraca Saldo

--- process_unit/src/app/neraca_saldo/neracaSaldo.services.js
+++ process_unit/src/app/neraca_saldo/neracaSaldo.services.js
@@ -2,7 +2,7 @@ import { LOGGER, logType } from "../../utils/loggerUtil.js"
 import { getSumOfStringValue } from "../../utils/mathUtil.js"
 import { convertJurnalUmumToNeracaSaldo } from "../../utils/neracaSaldoUtil.js"
 import { getJurnalUmumByBulanSebelumService } from "../jurnal_umum/jurnalUmum.services.js"
-import { getNeracaSaldoByBulanRepo, getFakturPenjualanBarangByBulanRepo, getReturPenjualanBarangByBulanRepo, getPelunasanPenjualanBarangByBulanRepo, getFakturPenjualanJasaByBulanRepo, getPembatalanPenjualanJasaByBulanRepo } from "./neracaSaldo.repository.js"
+import { getNeracaSaldoByBulanRepo } from "./neracaSaldo.repository.js"
 
 export const getAllNeracaSaldoByBulanService = async (bulan, tahun, whereIN, req_identity) => {
     bulan = parseFloat(bulan) < 10 ? "0" + bulan : bulan
@@ -15,7 +15,7 @@ export const getAllNeracaSaldoByBulanService = async (bulan, tahun, whereIN, req
         const getHistory = historyAkuns.filter((history) => history.kode_akun_perkiraan_code == item.kode_akun_perkiraan_code)
         const index = historyAkuns.indexOf(getHistory.length > 0 ? getHistory[0] : null)
         if (index >= 0) {
-            historyAkuns[index].sum_result = getSumOfStringValue([historyAkuns[index].sum_result, item.sum_result]).toFixed(2)
+            historyAkuns[index].sum_result = getSumOfStringValue([historyAkuns[index].sum_result, item.sum_result])
         }else{
             item.sum_result = item.sum_result.toFixed(2);
             historyAkuns.push(item)
