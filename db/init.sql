0ebd4ea6f5c76b75bf025cac6d05c9d4285bb506
erik1498 <djefrisonferdinan@gmail.com>
2024-05-31 23:01:54 +0800

    Update Docker Configuration

--- db/init.sql
+++ db/init.sql
@@ -2,8 +2,12 @@ CREATE DATABASE IF NOT EXISTS db_keps_assistant_initial;
 CREATE DATABASE IF NOT EXISTS db_keps_assistant_alor;
 CREATE DATABASE IF NOT EXISTS db_keps_assistant_keps;
 
-GRANT ALL PRIVILEGES ON db_keps_assistant_initial.* TO 'root'@'localhost' IDENTIFIED BY 'kepsAssistantDatabase123@#%__';
-GRANT ALL PRIVILEGES ON db_keps_assistant_alor.* TO 'root'@'localhost' IDENTIFIED BY 'kepsAssistantDatabase123@#%__';
-GRANT ALL PRIVILEGES ON db_keps_assistant_keps.* TO 'root'@'localhost' IDENTIFIED BY 'kepsAssistantDatabase123@#%__';
+CREATE USER 'kepsAssistantUserDB123@#%__'@'172.30.0.3' IDENTIFIED BY 'kepsAssistantPassDB123@#%__';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'kepsAssistantDatabasePassword123@#%__';
+
+GRANT SELECT, UPDATE ON db_keps_assistant_initial.* TO 'kepsAssistantUserDB123@#%__'@'172.30.0.3' IDENTIFIED BY 'kepsAssistantPassDB123@#%__';
+GRANT SELECT, UPDATE ON db_keps_assistant_alor.* TO 'kepsAssistantUserDB123@#%__'@'172.30.0.3' IDENTIFIED BY 'kepsAssistantPassDB123@#%__';
+GRANT SELECT, UPDATE ON db_keps_assistant_keps.* TO 'kepsAssistantUserDB123@#%__'@'172.30.0.3' IDENTIFIED BY 'kepsAssistantPassDB123@#%__';
 
 FLUSH PRIVILEGES;
