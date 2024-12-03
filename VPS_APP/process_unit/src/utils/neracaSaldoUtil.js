06e8f8f8374ba176a15a146ed1f7c91c484a6835
erik1498 <djefrisonferdinan@gmail.com>
2024-12-03 10:01:21 +0800

    Update Neraca Saldo

--- VPS_APP/process_unit/src/utils/neracaSaldoUtil.js
+++ VPS_APP/process_unit/src/utils/neracaSaldoUtil.js
@@ -47,7 +47,7 @@ export const getNeracaSaldoReport = (data) => {
                 kode_akun_perkiraan_code: data[i].kode_akun_perkiraan_code,
                 kode_akun_perkiraan_type: data[i].kode_akun_perkiraan_type,
                 debet: parseFloat(data[i].sum_result) >= 0 ? data[i].sum_result : 0,
-                kredit: parseFloat(data[i].sum_result) < 0 ? Math.abs(data[i].sum_result) : 0,
+                kredit: parseFloat(data[i].sum_result) < 0 ? Math.abs(data[i].sum_result).toFixed(2) : 0,
             })
         }
         res(result.sort((a, b) => {
