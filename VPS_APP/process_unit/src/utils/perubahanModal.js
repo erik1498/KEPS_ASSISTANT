fe0799dfbd42275d52188a43426ed21ddb760737
erik1498 <djefrisonferdinan@gmail.com>
2024-12-16 14:38:01 +0800

    Update Perubahan Modal

--- VPS_APP/process_unit/src/utils/perubahanModal.js
+++ VPS_APP/process_unit/src/utils/perubahanModal.js
@@ -1,5 +1,4 @@
-import { parseToRupiahText } from "./numberParsingUtil.js"
-import { ASSET_MINUS_KODE_AKUN, DEBET_POSITIF_TYPE, KREDIT_POSITIF_TYPE, EKUITAS_MINUS_KODE_AKUN, UTANG_MINUS_KODE_AKUN, generateCountValue, generateCountValueByPositifType, generateReportValue, generateReportValueByMinusValue, generateReportValueByPositifType } from "./validateKreditDebetTypeUtil.js"
+import { generateCountValue, generateReportValue, generateReportValueByMinusValue } from "./validateKreditDebetTypeUtil.js"
 
 
 export const MODAL_TYPE = "Modal"
@@ -11,7 +10,7 @@ export const getPerubahanModalReport = (data, laba_rugi_data_berjalan) => {
 
         for (let i = 0; i < data.length; i++) {
             if (data[i].kode_akun_perkiraan_type == MODAL_TYPE) {
-                let kredit = data[i].kredit > 0 ? `( ${parseToRupiahText(Math.abs(data[i].kredit))} )` : data[i].kredit
+                // let kredit = data[i].kredit > 0 ? `( ${parseToRupiahText(Math.abs(data[i].kredit))} )` : data[i].kredit
                 resultModal.push({
                     ...data[i],
                     value: generateReportValue(data[i], EKUITAS_MINUS_KODE_AKUN)
@@ -22,8 +21,8 @@ export const getPerubahanModalReport = (data, laba_rugi_data_berjalan) => {
         let labaTahunBerjalan = null
         if (laba_rugi_data_berjalan.laba_rugi.gain) {
             labaTahunBerjalan = {
-                kode_akun_perkiraan_code: "304",
-                kode_akun_perkiraan_name: "Laba/Rugi Periode Berjalan",
+                kode_akun_perkiraan_code: "003.099",
+                kode_akun_perkiraan_name: "LABA/RUGI PERIODE BERJALAN",
                 kode_akun_perkiraan_type: MODAL_TYPE,
                 debet: laba_rugi_data_berjalan.laba_rugi.gain,
                 kredit: 0,
@@ -35,8 +34,8 @@ export const getPerubahanModalReport = (data, laba_rugi_data_berjalan) => {
         }
         if (laba_rugi_data_berjalan.laba_rugi.loss) {
             labaTahunBerjalan = {
-                kode_akun_perkiraan_code: "304",
-                kode_akun_perkiraan_name: "Laba/Rugi Periode Berjalan",
+                kode_akun_perkiraan_code: "003.099",
+                kode_akun_perkiraan_name: "LABA/RUGI PERIODE BERJALAN",
                 kode_akun_perkiraan_type: MODAL_TYPE,
                 debet: 0,
                 kredit: Math.abs(laba_rugi_data_berjalan.laba_rugi.loss),
